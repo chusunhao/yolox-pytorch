bce744afc0c840953d36e96368722a60802109f7
chusunhao <Chu1220@sjtu.edu.cn>
2022-07-20 09:07:37 +0200

    modify networks

--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -2,8 +2,79 @@ import os
 
 import torch
 from tqdm import tqdm
+import numpy as np
 
 from utils.utils import get_lr
+import utils.se3lib as se3lib
+
+# def recoverXYZ(self, bbox, offset, depth):
+#     K = torch.tensor(
+#         [[1744.92206139719, 0, 737.272795902663], [0, 1746.58640701753, 528.471960188736], [0, 0, 1]]).cuda()
+#     b, num_anchors, _ = bbox.shape
+#     p = torch.concat(((bbox[..., 0:2] + offset), torch.ones_like(depth)), dim=-1)
+#     P = torch.matmul(torch.linalg.inv(K).repeat(b * num_anchors, 1, 1), (depth.repeat(1, 1, 3) * p).view(-1, 3, 1))
+#     return P.view(b, num_anchors, 3)
+
+
+# def decode_outputs(outputs, input_shape):
+#     grids = []
+#     strides = []
+#     hw = [x.shape[-2:] for x in outputs]
+#     # ---------------------------------------------------#
+#     #   outputs输入前代表每个特征层的预测结果
+#     #   batch_size, 4 + 1 + num_classes, 80, 80 => batch_size, 4 + 1 + num_classes, 6400
+#     #   batch_size, 5 + num_classes, 40, 40
+#     #   batch_size, 5 + num_classes, 20, 20
+#     #   batch_size, 4 + 1 + num_classes, 6400 + 1600 + 400 -> batch_size, 4 + 1 + num_classes, 8400
+#     #   堆叠后为batch_size, 8400, 5 + num_classes
+#     # ---------------------------------------------------#
+#     outputs = torch.cat([x.flatten(start_dim=2) for x in outputs], dim=2).permute(0, 2, 1)
+#     # ---------------------------------------------------#
+#     #   获得每一个特征点属于每一个种类的概率
+#     # ---------------------------------------------------#
+#     outputs[:, :, 4:6] = torch.sigmoid(outputs[:, :, 4:6])
+#     for h, w in hw:
+#         # ---------------------------#
+#         #   根据特征层的高宽生成网格点
+#         # ---------------------------#
+#         grid_y, grid_x = torch.meshgrid([torch.arange(h), torch.arange(w)])
+#         # ---------------------------#
+#         #   1, 6400, 2
+#         #   1, 1600, 2
+#         #   1, 400, 2
+#         # ---------------------------#
+#         grid = torch.stack((grid_x, grid_y), 2).view(1, -1, 2)
+#         shape = grid.shape[:2]
+#
+#         grids.append(grid)
+#         strides.append(torch.full((shape[0], shape[1], 1), input_shape[0] / h))
+#     # ---------------------------#
+#     #   将网格点堆叠到一起
+#     #   1, 6400, 2
+#     #   1, 1600, 2
+#     #   1, 400, 2
+#     #
+#     #   1, 8400, 2
+#     # ---------------------------#
+#     grids = torch.cat(grids, dim=1).type(outputs.type())
+#     strides = torch.cat(strides, dim=1).type(outputs.type())
+#     # ------------------------#
+#     #   根据网格点进行解码
+#     # ------------------------#
+#     outputs[..., :2] = (outputs[..., :2] + grids) * strides
+#     outputs[..., 2:4] = torch.exp(outputs[..., 2:4]) * strides
+#
+#
+#     outputs[:, :, 6:9] = recoverXYZ(outputs[..., :2], outputs[:, :, 6:8], outputs[:, :, 8:9])
+#     gt_r = se3lib.compute_rotation_matrix_from_quaternion(target)
+#     est_r = se3lib.symmetric_orthogonalization(pred)
+#
+#     # -----------------#
+#     #   归一化
+#     # -----------------#
+#     # outputs[..., [0, 2]] = outputs[..., [0, 2]] / input_shape[1]
+#     # outputs[..., [1, 3]] = outputs[..., [1, 3]] / input_shape[0]
+#     return outputs
 
 
 def fit_one_epoch(model_train, model, ema, yolo_loss, loss_history, eval_callback, optimizer, epoch, epoch_step,
@@ -52,7 +123,7 @@ def fit_one_epoch(model_train, model, ema, yolo_loss, loss_history, eval_callbac
                 # ----------------------#
                 #   计算损失
                 # ----------------------#
-                loss_value = yolo_loss(outputs, targets)
+                loss_value = yolo_loss(outputs, targets, poses)
 
             # ----------------------#
             #   反向传播
@@ -81,7 +152,7 @@ def fit_one_epoch(model_train, model, ema, yolo_loss, loss_history, eval_callbac
     else:
         model_train_eval = model_train.eval()
 
-    stats = np.zeros((len(gen_val), 3))
+    # stats = np.zeros((len(gen_val), 3))
 
     for iteration, batch in enumerate(tqdm(gen_val)):
         if iteration >= epoch_step_val:
@@ -103,18 +174,20 @@ def fit_one_epoch(model_train, model, ema, yolo_loss, loss_history, eval_callbac
             # ----------------------#
             #   计算损失
             # ----------------------#
-            loss_value = yolo_loss(outputs, targets)
+            loss_value = yolo_loss(outputs, targets, poses)
 
-            # Evaluate error
-            posit_err, rel_posit_err, orient_err = pose_err(est_pose, gt_pose)
-
-            # Collect statistics
-            stats[val_samples - batch_size:val_samples, 0] = posit_err.cpu().numpy()
-            stats[val_samples - batch_size:val_samples, 1] = orient_err.cpu().numpy()
-            stats[val_samples - batch_size:val_samples, 2] = rel_posit_err.cpu().numpy()
-
-            pbar.desc = "Pose error: {:.3f}[m], {:.3f}[deg]".format(
-                posit_err.mean().item(), orient_err.mean().item())
+            # outputs = decode_outputs(outputs, list(images.shape[-2:]))
+            #
+            # # Evaluate error
+            # posit_err, rel_posit_err, orient_err = pose_err(est_pose, gt_pose)
+            #
+            # # Collect statistics
+            # stats[val_samples - batch_size:val_samples, 0] = posit_err.cpu().numpy()
+            # stats[val_samples - batch_size:val_samples, 1] = orient_err.cpu().numpy()
+            # stats[val_samples - batch_size:val_samples, 2] = rel_posit_err.cpu().numpy()
+            #
+            # pbar.desc = "Pose error: {:.3f}[m], {:.3f}[deg]".format(
+            #     posit_err.mean().item(), orient_err.mean().item())
 
         val_loss += loss_value.item()
         if local_rank == 0:
